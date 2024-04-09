822e1689d555f2255ba5583aac5c8936e0a088d6
Artem Ushenko <99095504+Artem-Ushenko@users.noreply.github.com>
2024-04-09 12:52:41 +0300

    Feature/terraform (#1)
    * Added Terraform files
    
    * Update added outputs.tf
    
    * Terraform files update
    
    * Updated Terraform files
    
    * Revert "Updated Terraform files"
    
    This reverts commit e262856afd2b8fe5f4e4c9e169fdcec3cfdb1b1d.
    
    * Update

---
+++ terraform/outputs.tf
@@ -0,0 +1,13 @@
+output "public_ip" {
+  value = aws_instance.ec2.public_ip
+}
+output "public_dns" {
+  value = aws_instance.ec2.public_dns
+}
+output "db_endpoint" {
+  value = aws_db_instance.db.endpoint
+}
+output "db_password" {
+  value     = random_password.password.result
+  sensitive = true
+}
