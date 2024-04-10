46fb024da298b31791447da2f61d584ec01c9d2c
Artem Ushenko <kingart030192kingart@gmail.com>
2024-04-10 17:05:43 +0300

    Update workflow to GitHub Actions and outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,9 +6,11 @@ output "public_dns" {
 }
 output "db_endpoint" {
   value = aws_db_instance.db.endpoint
+  sensitive = true
 }
 output "db_username" {
   value = aws_db_instance.db.username
+  sensitive = true
 }
 output "db_password" {
   value     = random_password.password.result
@@ -16,6 +18,7 @@ output "db_password" {
 }
 output "redis_endpoint" {
   value = aws_elasticache_cluster.redis.cache_nodes.0.address
+  sensitive = true
 }
 output "redis_port" {
   value = aws_elasticache_cluster.redis.cache_nodes.0.port
