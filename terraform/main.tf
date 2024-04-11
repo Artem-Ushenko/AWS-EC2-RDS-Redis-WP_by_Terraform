b4beffc21725218bd4d1346f56883fac19dfc49a
Artem Ushenko <kingart030192kingart@gmail.com>
2024-04-11 14:58:15 +0300

    Update Terraform files

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,6 +33,7 @@ resource "aws_db_instance" "db" {
 # Create elasticache instance in private subnet
 resource "aws_elasticache_cluster" "redis" {
   cluster_id           = var.cluster_id
+  pasword              = random_password.password.result
   engine               = "redis"
   node_type            = var.node_type
   num_cache_nodes      = 1
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,12 @@
+# Output EC2 instance public
 output "public_ip" {
   value = aws_instance.ec2.public_ip
 }
 output "public_dns" {
   value = aws_instance.ec2.public_dns
 }
+
+# Output database
 output "db_name" {
   value = aws_db_instance.db.db_name
 }
@@ -19,9 +22,14 @@ output "db_password" {
   value     = random_password.password.result
   sensitive = true
 }
+
+# Output redis
 output "redis_endpoint" {
   value     = aws_elasticache_cluster.redis.cache_nodes.0.address
   sensitive = true
+}
+output "redis_password" {
+  value = random_password.password.result
 }
 output "redis_port" {
   value = aws_elasticache_cluster.redis.cache_nodes.0.port
